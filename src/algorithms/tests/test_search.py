72ad31e65552bf42b20c75fcd72b930d9af9a442
EXT02D22861 <nusret.ozates@consultant.turkcell.com.tr>
2022-04-02 13:16:13 +0200

    Better looking tests

--- src/algorithms/tests/test_search.py
+++ src/algorithms/tests/test_search.py
@@ -1,51 +1,58 @@
+from unittest import TestCase
+from typing import Union
+import enum
 from ..search.binary import binary_search
 from ..search.recursive_binary import recursive_binary_search
 from ..search.linear import linear_search
 
 
-def test_linear_search():
-    result = linear_search([1, 2, 3, 4, 5, 6], 2)
-    assert result is not None
-
-    result = linear_search([1, 2, 3, 4, 5, 6], 8)
-    assert result is None
-
-    result = linear_search([], 5)
-    assert result is None
-
-    result = linear_search([1, 2, 3, 4, 5], None)
-    assert result is None
+class Result(enum.Enum):
+    SUCCESS = "success"
+    FAILURE = "failure"
 
 
-def test_binary_search():
-    result = binary_search([1, 2, 3, 4, 5, 6], 2)
-    assert result == 1
+class AcceptanceThreshold:
+    def __init__(self, result: Union[int, None]) -> None:
+        self._result = result
 
-    result = binary_search([1, 2, 3, 4, 5, 6], 5)
-    assert result == 4
+    def status(self):
+        if self._result is None or self._result is False:
+            return Result.FAILURE
 
-    result = binary_search([1, 2, 3, 4, 5, 6], 8)
-    assert result is None
-
-    result = binary_search([], 5)
-    assert result is None
-
-    result = binary_search([1, 2, 3, 4, 5], None)
-    assert result is None
+        return Result.SUCCESS
 
 
-def test_recursive_binary_search():
-    result = recursive_binary_search([1, 2, 3, 4, 5, 6], 2)
-    assert result
+class TestSearch(TestCase):
+    def setUp(self) -> None:
+        self.linear_test_data = (
+            (([1, 2, 3, 4, 5, 6], 2), Result.SUCCESS),
+            (([1, 2, 3, 4, 5, 6], 8), Result.FAILURE),
+            (([], 5), Result.FAILURE),
+            (([1, 2, 3, 4, 5], None), Result.FAILURE),
+        )
 
-    result = recursive_binary_search([1, 2, 3, 4, 5, 6], 5)
-    assert result
+        self.binary_test_data = (
+            (([1, 2, 3, 4, 5, 6], 2), Result.SUCCESS),
+            (([1, 2, 3, 4, 5, 6], 5), Result.SUCCESS),
+            (([1, 2, 3, 4, 5, 6], 8), Result.FAILURE),
+            (([], 5), Result.FAILURE),
+            (([1, 2, 3, 4, 5], None), Result.FAILURE),
+        )
 
-    result = recursive_binary_search([1, 2, 3, 4, 5, 6], 8)
-    assert not result
+    def test_linear_search(self):
+        for test_data, expected in self.linear_test_data:
+            with self.subTest(test_data=test_data):
+                result = AcceptanceThreshold(linear_search(*test_data)).status()
+                self.assertEqual(result, expected)
 
-    result = recursive_binary_search([], 5)
-    assert not result
+    def test_binary_search(self):
+        for test_data, expected in self.binary_test_data:
+            with self.subTest(test_data=test_data):
+                result = AcceptanceThreshold(binary_search(*test_data)).status()
+                self.assertEqual(result, expected)
 
-    result = recursive_binary_search([1, 2, 3, 4, 5], None)
-    assert not result
+    def test_recursive_binary_search(self):
+        for test_data, expected in self.binary_test_data:
+            with self.subTest(test_data=test_data):
+                result = AcceptanceThreshold(recursive_binary_search(*test_data)).status()
+                self.assertEqual(result, expected)
